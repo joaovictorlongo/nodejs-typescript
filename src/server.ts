1beb4cc636ea4ef5359f83a56017bf054d666ad7
Joao Victor <joaovictorlongoo@gmail.com>
2021-09-16 15:22:28 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+  console.log('Server initialized on port 3333!🚀');
+});
